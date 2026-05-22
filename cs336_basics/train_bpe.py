d95a9bf89aadab2644fb2b5d364e84897cc5b666
Guxin Jin <gjin@meta.com>
2026-05-22 07:38:31 -0700

    train_bpe brute force -- TODO: update sequency frequency and rebuild count pairs

--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -1,6 +1,7 @@
 import os
 from collections import Counter
 import regex
+from pathlib import Path
 
 GPT2_PATTERN = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 
@@ -26,6 +27,86 @@ def pretokenize(text: str, special_tokens: list[str]) -> Counter[tuple[bytes, ..
             counts[atom] += 1
 
     return counts
+
+def get_pair_count_from_word(word: tuple[bytes, ...]) -> dict[tuple[bytes, bytes], int]:
+    """
+    Count adjacent pair count from a single word
+    """
+    counts: dict[tuple[bytes, bytes], int] = {}
+    if len(word < 2):
+        return pair_counts
+    prev = word[0]
+    for w in word[1:]:
+        pair = (prev, w)
+        counts[p] = counts.get(p, 0) + 1
+        prev = w
+    return counts
+
+
+def build_pair_counts(word_freq: Counter[tuple[bytes, ...]]) -> dict[tuple[bytes, bytes], int]:
+    """
+    Returns:
+    pair_counts: adjacent bytes pair counting
+    """
+
+    pair_counts: dict[tuple[bytes, bytes], int] = {}
+    for word, freq in word_freq.items():
+        if len(word) < 2:
+            continue
+        local_pair_counts: dict[tuple[bytes, bytes], int]  = get_pair_count_from_word(word)
+        for pair, count in local_pair_counts.items():
+            pair_counts[pair] = pair_counts.get(pair, 0) + count * freq
+
+    return pair_counts
+
+def train_bpe(
+    input_path: str | os.PathLike, 
+    vocab_size: int, 
+    special_tokens: list[str]) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+    
+    """
+    Train BPE
+    
+    Returns:
+    vocab: dict[int, bytes]
+    merges: list[tuple[bytes, bytes]]
+    """
+
+    # vocab init: 256 single byte token + special tokens 
+    vocab: dict[int, bytes] = {i: bytes([i]) for i in range(256)}
+    next_id = 256
+    for st in special_tokens:
+        vocab[next_id] = st.encode("utf-8")
+        next_id += 1
+
+
+    # pretokenize
+    text: str = Path(input_path).read_text(encoding="utf-8")
+    word_seq_freq: Counter[tuple[bytes, ...]] = pretokenize(text)
+
+    # BPE merge
+    pair_counts: dict[tuple[bytes, bytes], int] = build_pair_counts(word_seq_freq)
+    merges: List[tuple[bytes, bytes]] = []
+
+    while next_id < vocab_size:
+        if not pair_counts:
+            break
+        # choose best pair
+        (a, b), best_count = max(pair_counts.items(), key=lambda item: (item[1], item[0]))
+        if best_count <= 0:
+            break
+
+        new_token = a + b
+        merges.append((a, b))
+        vocab[next_id] = new_token
+        next_id += 1
+
+        ## 
+
+        
+
+
+
 
 
 def run_train_bpe(
